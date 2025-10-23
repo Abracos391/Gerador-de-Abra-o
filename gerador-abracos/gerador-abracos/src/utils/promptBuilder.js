3c848cd5dd6ef96a277437f4b949bb7ecd74c638
Abracos391 <folmadmetal@gmail.com>
2025-10-23 18:39:51 -0300

    Delete gerador-abracos/gerador-abracos directory

--- gerador-abracos/gerador-abracos/src/utils/promptBuilder.js
+++
@@ -1,5 +0,0 @@
-export function buildPrompt({ mensagem, estilo, tema, corPredominante, corTexto, posicaoTexto }) {
-  return `Crie uma imagem em formato retrato (1024x1536) com estilo ${estilo}, fundo ${tema}, cor predominante ${corPredominante}. 
-Insira o texto "${mensagem}" com cor ${corTexto}, posicionado no ${posicaoTexto} da imagem. 
-A imagem deve ter aparência nítida, harmoniosa e apropriada para uso como wallpaper ou figurinha personalizada.`;
-}
